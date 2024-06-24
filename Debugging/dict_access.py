d087648c5dd3ba964a0d95e31439b83bd32acb07
Jerome <serplord@gmail.com>
2024-06-24 12:10:05 +0200

    completed debugging

---
+++ Debugging/dict_access.py
@@ -0,0 +1,12 @@
+'''
+You are trying to access a value in a dictionary, 
+but the code is giving you an error. Can you change line 3 
+so that it prints "Unknown" instead of raising an error?
+'''
+
+info = {'name': 'Srdjan', 'age': 38}
+
+#get()method to set a default return
+print(info.get('city', 'Unknown'))
+
+
